6c4be9e6dd6847ab5def10f2f0eb681f48493d29
Gary Sieling <gary@garysieling.com>
2018-10-27 22:16:43 -0400

    add vtt test

--- test/test.js
+++ test/test.js
@@ -7,6 +7,35 @@ describe('Array', function() {
     const text = `WEBVTT
 Kind: captions
 Language: en
+Style:
+::cue(c.colorCCCCCC) { color: rgb(204,204,204);
+ }
+::cue(c.colorE5E5E5) { color: rgb(229,229,229);
+ }
+##
+
+00:00:00.060 --> 00:00:02.360 align:start position:0%
+
+if<00:00:00.240><c> you</c><00:00:00.420><c> take</c><00:00:00.630><c> a</c><00:00:00.659><c> store</c><00:00:01.170><c> and</c><c.colorE5E5E5><00:00:01.469><c> put</c><00:00:01.650><c> it</c><00:00:01.740><c> inside</c><00:00:01.829><c> a</c></c>
+
+00:00:02.360 --> 00:00:02.370 align:start position:0%
+if you take a store and<c.colorE5E5E5> put it inside a
+ </c>
+`
+
+	  it('vtt', function() {
+      assert.equal('In the last video, we saw that something happened.', srt.parse(text));
+    });
+  });
+});
+
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+
+    const text = `WEBVTT
+Kind: captions
+Language: en
 	  
 00:00:00.610 --> 00:00:02.640
 In the last video, we
