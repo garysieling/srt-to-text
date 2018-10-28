eaf43cd4ec122a96594d9e6c02f3024a0da3884e
Gary Sieling <gary@garysieling.com>
2018-10-27 21:52:27 -0400

    Remove headers

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 exports.parse = function(text) {
+  if (text.match(/^WEBVTT\n/)) {
+    text = text.substring("WEBVTT\n".length)
+  }
+
+  if (text.match(/^Kind: captions\n/)) {
+    text = text.substring("Kind: captions\n".length)
+  }
+
+  if (text.startsWith("Language: ")) {
+    text = text.substring(text.indexOf("\n"));
+  }
+
   let lines = text.split("\n");
   let matchBreak = /\d\d:\d\d:\d\d\.\d\d\d --> \d\d:\d\d:\d\d\.\d\d\d/i;
   let transcript = "";
--- test/test.js
+++ test/test.js
@@ -4,6 +4,29 @@ const srt = require('../index');
 describe('Array', function() {
   describe('#indexOf()', function() {
 
+    const text = `WEBVTT
+Kind: captions
+Language: en
+	  
+00:00:00.610 --> 00:00:02.640
+In the last video, we
+
+00:10:04.340 --> 00:10:06.340
+saw that
+
+00:10:06.340 --> 00:10:08.850
+something happened.`
+
+	  it('headers', function() {
+      assert.equal('In the last video, we saw that something happened.', srt.parse(text));
+    });
+  });
+});
+
+
+describe('Array', function() {
+  describe('#indexOf()', function() {
+
     const text = `00:00:00.610 --> 00:00:02.640
 In the last video, we
 
